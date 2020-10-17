6ef0e9797a175dc79469fa0039069529275392ee
mohamadreza <mohamad.r.sadeghi93@gmail.com>
2020-10-17 03:38:39 +0330

    validation

--- src/coffees/dto/create-coffees.dto.ts
+++ src/coffees/dto/create-coffees.dto.ts
@@ -1,5 +1,9 @@
+import { IsString } from 'class-validator';
 export class CreateCoffeesDto {
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly brand: string;
+  @IsString({ each: true })
   readonly flavor: Array<string>;
 }
--- src/coffees/dto/update-coffees.dto.ts
+++ src/coffees/dto/update-coffees.dto.ts
@@ -1,5 +1,10 @@
-export class UpdateCoffeesDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavor?: Array<string>;
-}
+// export class UpdateCoffeesDto {
+//   readonly name?: string;
+//   readonly brand?: string;
+//   readonly flavor?: Array<string>;
+// }
+
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeesDto } from './create-coffees.dto';
+
+export class UpdateCoffeesDto extends PartialType(CreateCoffeesDto) {}
