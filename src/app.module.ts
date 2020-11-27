d538758d73124d9ebedffa9d3131e4fb557646b5
mohamad_sdg <mohamad.r.sadeghi93@gmail.com>
2020-11-27 22:34:03 +0330

    setup .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 // import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database:  process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
