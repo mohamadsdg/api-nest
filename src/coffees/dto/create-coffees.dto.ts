3a37e2ec5e731255b76ffcb36be1fac380d98045
mohamad_sdg <mohamad.r.sadeghi93@gmail.com>
2020-12-08 02:48:39 +0330

    using Decorator for setup docs

--- src/coffees/dto/create-coffees.dto.ts
+++ src/coffees/dto/create-coffees.dto.ts
@@ -1,10 +1,15 @@
 import { IsString } from 'class-validator';
-// import {  } from "@nestjs/swagger";
+import { ApiProperty } from "@nestjs/swagger";
 export class CreateCoffeesDto {
+  @ApiProperty({description:'The name of coffee. '})
   @IsString()
   readonly name: string;
+
+  @ApiProperty({description:'The name of brand coffee. '})
   @IsString()
   readonly brand: string;
+
+  @ApiProperty({description:'list of flavor  ',default:[]})
   @IsString({ each: true })
   readonly flavor: Array<string>;
 }
--- src/coffees/dto/update-coffees.dto.ts
+++ src/coffees/dto/update-coffees.dto.ts
@@ -4,7 +4,8 @@
 //   readonly flavor?: Array<string>;
 // }
 
-import { PartialType } from '@nestjs/mapped-types';
+// import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCoffeesDto } from './create-coffees.dto';
 
 export class UpdateCoffeesDto extends PartialType(CreateCoffeesDto) {}
